6381dfd4388e0086491c3dc64ad82b0ab6643022
Pgfeliu <pamela.gfeliu@gmail.com>
2023-04-26 19:34:08 -0400

    Configuración Firebase

---
+++ app-notes/src/firebase/firebaseconfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth} from 'firebase/firebase-auth'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBa5NkK6Ayguo0z80BOU6dQE57uBfgZYuE",
+    authDomain: "app-notes-ab9ea.firebaseapp.com",
+    projectId: "app-notes-ab9ea",
+    storageBucket: "app-notes-ab9ea.appspot.com",
+    messagingSenderId: "177071837603",
+    appId: "1:177071837603:web:bf48b22c3e35697d2dca16",
+    measurementId: "G-5Z2GPWFZ06"
+  };
+//Iniciar Firebase
+export const firebaseApp = initializeApp(firebaseConfig)
+export const auth = getAuth(firebaseApp)
